60b828e4345d6660321354af3295cc1bfc69408b
harish-Immannavar <harishimmannavar6@gmail.com>
2026-07-08 14:19:56 +0530

    Changes in Prediction Models

--- pages/Stock_prediction.py
+++ pages/Stock_prediction.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pages.utils.model_train import get_data, get_differencing_order,scaling,evaluate_model,get_rolling_mean,get_forecast,inverse_scaling
+from pages.utils.model_train import get_data, get_differencing_order,scaling,get_rolling_mean,get_forecast,inverse_scaling, compare_models
 import pandas as pd
 from pages.utils.plotly_figures import plotly_table,Moving_average_forecast
 
@@ -14,43 +14,53 @@ col1,col2,col3 = st.columns(3)
 with col1:
     ticker = st.text_input('Stock ticker','AAPL')
 
-rmse = 0
+
 
 st.subheader('Predicting next 30 days close Price for:'+ticker)
 
-close_price = get_data(ticker)
-rolling_price = get_rolling_mean(close_price)
+close_price = get_data(ticker).squeeze()
+#rolling_price = get_rolling_mean(close_price.squeeze())
+print(close_price.head())
 
-differencing_order = get_differencing_order(rolling_price)
-scaled_data,scaler = scaling(rolling_price)
-scaled_data = scaled_data.flatten()
-rmse = evaluate_model(scaled_data,differencing_order)
+differencing_order = get_differencing_order(close_price)
+#scaled_data,scaler = scaling(rolling_price)
+#scaled_data = scaled_data.flatten()
+results, best_model = compare_models(
+    close_price
+)
+st.subheader("Model Comparison")
 
-st.write("**Model Rmse Score**",rmse)
-forecast = get_forecast(scaled_data,differencing_order)
-forecast['Close'] = inverse_scaling(
-    scaler,
-    forecast['Close']
-).flatten()
+results_df = pd.DataFrame(
+    results.items(),
+    columns=["Model","RMSE"]
+)
+
+st.dataframe(results_df)
+
+st.success(
+    f"Best Model : {best_model}"
+)
+
+
+forecast = get_forecast(close_price)
+#forecast['Close'] = inverse_scaling(scaler,forecast['Close']).flatten()
 st.write('##### Forecast Data(Next 30 Days)')
 fig_tail = plotly_table (forecast.sort_index(ascending=True).round(3))
 fig_tail.update_layout(height=220)
 st.plotly_chart(fig_tail,use_container_width=True)
 
 
-# Flatten column MultiIndex if present
-if isinstance(rolling_price.columns, pd.MultiIndex):
-    rolling_price.columns = rolling_price.columns.get_level_values(0)
-
-if isinstance(forecast.columns, pd.MultiIndex):
-    forecast.columns = forecast.columns.get_level_values(0)
 
 
 
-combined = pd.concat([rolling_price, forecast])
+
+combined = pd.concat([
+    close_price.to_frame(name="Close"),
+    forecast
+])
 
 
 st.plotly_chart(
-    Moving_average_forecast(combined.iloc[250:]),
+    Moving_average_forecast(combined.iloc[100:]),
     use_container_width=True
 )
--- pages/utils/model_train.py
+++ pages/utils/model_train.py
@@ -2,6 +2,8 @@ import yfinance as yf
 from statsmodels.tsa.stattools import adfuller
 from sklearn.metrics import mean_squared_error,r2_score
 from statsmodels.tsa.arima.model import ARIMA
+from prophet import Prophet
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from datetime import    datetime,timedelta
@@ -32,19 +34,153 @@ def get_differencing_order(close_price):
         else:
             break
     return d
+
+def get_best_order(data, d):
+    best_aic = float("inf")
+    best_order = (1, d, 1)
+
+    for p in range(6):
+        for q in range(6):
+            try:
+                model = ARIMA(data, order=(p, d, q))
+                result = model.fit()
+
+                if result.aic < best_aic:
+                    best_aic = result.aic
+                    best_order = (p, d, q)
+
+            except:
+                continue
+
+    return best_order
+
+def arima_model(train, test, order):
+    # Convert training data to a list so we can dynamically append to it
+    history = list(train.values)
+    predictions = []
+    
+    # Loop through every single day in our test set
+    for actual_price in test.values:
+        # Fit the model on our updated history
+        model = ARIMA(history, order=order)
+        model_fit = model.fit()
+        
+        # Forecast exactly 1 step ahead
+        output = model_fit.forecast(steps=1)
+        yhat = output[0]
+        predictions.append(yhat)
+        
+        # CRITICAL: Add the actual observed price back into our history for the next day
+        #history.append(actual_price)
+        
+    # Calculate RMSE based on our day-by-day rolling predictions
+    rmse = np.sqrt(mean_squared_error(test, predictions))
+    return rmse
+
+def sarima_model(train, test, order):
+
+    model = SARIMAX(
+        train,
+        order=order,
+        seasonal_order=(1,1,1,12)
+    )
+
+    model_fit = model.fit(disp=False)
+
+    prediction = model_fit.forecast(len(test))
+
+    rmse = np.sqrt(mean_squared_error(test, prediction))
+
+    return rmse
+
+
+def prophet_model(train, test):
+
+    train_df = pd.DataFrame({
+        "ds": train.index,
+        "y": train.values
+    })
+
+    model = Prophet(
+        daily_seasonality=True
+    )
+
+    model.fit(train_df)
+
+    future = model.make_future_dataframe(
+        periods=len(test),
+        freq="B"
+    )
+
+    forecast = model.predict(future)
+
+    prediction = forecast["yhat"].tail(len(test)).values
+
+    rmse = np.sqrt(
+        mean_squared_error(test.values, prediction)
+    )
+
+    return rmse
+
+def naive_baseline_model(train, test):
+    """
+    Predicts that every day in the test set will just be 
+    the very last closing price observed in the training set.
+    """
+    # Grab the last known price from the training data
+    last_known_price = train.iloc[-1]
+    
+    # Create an array of predictions that is just this price repeated
+    predictions = np.full(shape=len(test), fill_value=last_known_price)
+    
+    # Calculate RMSE
+    rmse = np.sqrt(mean_squared_error(test, predictions))
+    
+    return rmse
+
+
+def compare_models(close_price):
+
+    train = close_price[:-30]
+
+    test = close_price[-30:]
+
+    d = get_differencing_order(train)
+
+    order = get_best_order(train, d)
+
+    results = {}
+
     
 
-def fit_model(data,differencing_order):
-    model= ARIMA(data,order=(30,differencing_order ,30))
-    model_fit = model.fit()
+    results["ARIMA"] = arima_model(
+        train,
+        test,
+        order
+    )
 
-    forecast_steps = 30
-    forecast = model_fit.get_forecast(steps = forecast_steps)
+    results["SARIMA"] = sarima_model(
+        train,
+        test,
+        order
+    )
 
-    predictions = forecast.predicted_mean
-    return predictions
+    results["Prophet"] = prophet_model(
+        train,
+        test
+    )
 
-def evaluate_model(original_price,differencing_order):
+    results["Naive Baseline"] = naive_baseline_model(train, test)
+
+    best_model = min(
+        results,
+        key=results.get
+    )
+
+    return results, best_model
+
+
+#def evaluate_model(original_price,differencing_order):
     train_data,test_data = original_price[:-30],original_price[-30:]
     predictions= fit_model(train_data, differencing_order)
     rmse =  np.sqrt(mean_squared_error  (test_data,predictions))
@@ -55,14 +191,98 @@ def scaling(close_price):
     scaled_data = scaler.fit_transform( np.array(close_price).reshape(-1,1))
     return scaled_data,scaler
 
-def get_forecast(original_price,differencing_order):
-    preidictions = fit_model(original_price,differencing_order)
-    start_date = datetime.now().strftime('%Y-%m-%d')
-    end_date =  (datetime.now()+timedelta(days=29)).strftime('%Y-%m-%d')    
-    forecast_index = pd.date_range(start=start_date,end=end_date,freq='B')
-    forecast_df = pd.DataFrame(preidictions,index=forecast_index ,columns= ['Close'])
+def get_forecast(original_price):
+
+    results, best_model = compare_models(original_price)
+
+    last_date = original_price.index[-1]
+
+    forecast_index = pd.date_range(
+        start=last_date + pd.offsets.BDay(1),
+        periods=30,
+        freq="B"
+    )
+
+    if best_model == "ARIMA":
+
+        d = get_differencing_order(original_price)
+
+        order = get_best_order(
+            original_price,
+            d
+        )
+
+        model = ARIMA(
+            original_price,
+            order=order
+        )
+
+        model_fit = model.fit()
+
+        prediction = model_fit.forecast(
+            steps=30
+        )
+
+
+    elif best_model == "SARIMA":
+
+        d = get_differencing_order(original_price)
+
+        order = get_best_order(
+            original_price,
+            d
+        )
+
+        model = SARIMAX(
+            original_price,
+            order=order,
+            seasonal_order=(1,1,1,12)
+        )
+
+        model_fit = model.fit(disp=False)
+
+        prediction = model_fit.forecast(
+            steps=30
+        )
+
+
+    elif best_model == "Prophet":
+
+        train_df = pd.DataFrame({
+            "ds": original_price.index,
+            "y": original_price.values
+        })
+
+        model = Prophet(
+            daily_seasonality=True
+        )
+
+        model.fit(train_df)
+
+
+        future = model.make_future_dataframe(
+            periods=30,
+            freq="B"
+        )
+
+        forecast = model.predict(future)
+
+        prediction = forecast["yhat"].tail(30).values
+
+
+
+    forecast_df = pd.DataFrame(
+        {
+            "Close": prediction.values
+        },
+        index=forecast_index
+    )
+
+
     return forecast_df
+
+    
 
 def inverse_scaling(scaler,scaled_data):
     close_price= scaler.inverse_transform(np.array(scaled_data).reshape(-1,1))
-    return close_price
+    return close_price  
